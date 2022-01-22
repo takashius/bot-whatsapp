e1cb222c79354c7c3357f01d22ee612cffa13928
Erick Hernandez <takashi.onimaru@gmail.com>
2022-01-21 23:05:17 -0400

    puerto 3000

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ webApp.use(bodyParser.urlencoded({
 webApp.use(bodyParser.json());
 webApp.use(cors());
 
-const PORT = 80;
+const PORT = 3000;
 
 webApp.get('/', (req, res) => {
   res.send(`Hello World.!`);
